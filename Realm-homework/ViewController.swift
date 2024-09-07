b95e1c6245ddeb058da9526464b6a1c929fed7cf
Kirill <onlyjesus13@icloud.com>
2024-09-07 18:13:29 +0500

    Initial Commit

---
+++ Realm-homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Realm-homework
+//
+//  Created by prom1 on 07.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
